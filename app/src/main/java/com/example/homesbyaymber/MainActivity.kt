d0608a87ddc78063d4a533013fb6ceb16e16ffb7
Arrington Perry <perryceo21@hotmail.com>
2022-09-21 21:59:50 -0400

    Main page setup

--- app/src/main/java/com/example/homesbyaymber/MainActivity.kt
+++ app/src/main/java/com/example/homesbyaymber/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.homesbyaymber
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.example.homesbyaymber.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), NavFrag {
+
+    private lateinit var bind : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        //Switch to main page
+        bind.guestBTN.setOnClickListener {
+            fragNav(HomesFragment(), false)
+        }
     }
 
-//Fragment transition interface
     override fun fragNav(fragment: Fragment, addToStack: Boolean) {
-        val transaction = supportFragmentManager
-            .beginTransaction()
+        val transaction = supportFragmentManager.beginTransaction()
             .replace(R.id.main, fragment)
         if(addToStack){
             transaction.addToBackStack(null)
